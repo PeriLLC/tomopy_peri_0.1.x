bd96397a6785e89e7bb059629067c8ff179d535d
dakefeng <dakefeng@gmail.com>
2015-07-30 21:57:59 -0400

    make file oked, can build library from setup.py, but not packed into egg with right format.

--- setup.py
+++ setup.py
@@ -18,201 +18,84 @@
 #    You should have received a copy of the GNU Lesser General Public License
 #    along with TomograPeri.  If not, see <http://www.gnu.org/licenses/>.
 
-#from distutils.core import setup, Extension
-from setuptools import setup, Extension, find_packages
-
-from distutils.command.build_ext import build_ext
-from distutils.command.clean import clean
 import os
-import platform
-import re
-import subprocess
-import sys
-import zlib
+from sys import platform
+from setuptools import setup, Extension, find_packages
+from setuptools.command.install import install
+from distutils.command.build import build
+from subprocess import call
+
+VERSION = '0.1.1' 
 
 
-VERSION = '0.1.0' 
 
-NVIDIA_INC_DIRS = []
-NVCC = 'nvcc'
-CUDALIB = '/usr/cuda/lib64'
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
+TOMOPERI_PATH = os.path.join(BASEPATH, 'src/xeon_phi')
 
-INTEL_INC_DIRS = []
-ICPC = 'icpc'
-ICPCLIB = '/opt/intel/composerxe/lib/intel64'
 
-cuda_found = False
-icpc_found = False
+class TomoPeriBuild(build):
+    def run(self):
+        # run original build code
+        build.run(self)
 
-for path in ('/usr/local/cuda', '/opt/cuda'):
-    if os.path.exists(path):
-        NVIDIA_INC_DIRS.append(os.path.join(path, 'include'))
-        NVCC = os.path.join(path, 'bin', 'nvcc')
-        CUDALIB = os.path.join(path, 'lib64')
-        cuda_found = True
-        break
+        # build TomoPeri
+        build_path = os.path.abspath(self.build_temp)
 
-for path in ('/opt/local/intel/composerxe', '/opt/intel/composerxe'):
-    if os.path.exists(path):
-        INTEL_INC_DIRS.append(os.path.join(path, 'include'))
-        ICPC = os.path.join(path, 'bin', 'icpc')
-        ICPCLIB = os.path.join(path, 'lib/intel64')
-        icpc_found = True
-        break
+        cmd = [
+            'make',
+            'OUT=' + build_path,
+            'V=' + str(self.verbose),
+        ]
 
-if not (cuda_found or icpc_found):
-    raise ValueError( "The CUDA and ICPC compiler and headers required to build " \
-                       "kernel were not found. Cannot build library..." )
+        options = [
+            'DEBUG=n',
+            'ENABLE_SDL=n',
+        ]
+        cmd.extend(options)
 
-CUDA_EXTRA_COMPILE_ARGS = ['-Wall', '-fno-strict-aliasing', \
-                      '-DVERSION="%s"' % (VERSION,)]
+        targets = ['test']
+        cmd.extend(targets)
 
-ICPC_EXTRA_COMPILE_ARGS = ['-ldl', '-shared']
+        target_files = [os.path.join(build_path, 'libtomoperi_phi.so'),os.path.join(build_path, 'tomoperi_phi_test.exe')]
 
-class GPUBuilder(build_ext):
 
-    def _call(self, comm):
-        p = subprocess.Popen(comm, stdout=subprocess.PIPE, shell=True)
-        stdo, stde = p.communicate()
-        if p.returncode == 0:
-            return stdo
-        else:
-            print >>sys.stderr, "%s\nFailed to execute command '%s'" % \
-                                (stde, comm)
-            return None
+        print cmd
 
-    def _makedirs(self, pathname):
-        try:
-            os.makedirs(pathname)
-        except OSError:
-            pass
+        def compile():
+            call(cmd, cwd=TOMOPERI_PATH)
+
+        self.execute(compile, [], 'Compiling Tomopy_Peri on Phi')
+
+        # copy resulting tool to library build folder
+        self.mkpath(self.build_lib)
+
+        if not self.dry_run:
+            for target in target_files:
+                self.copy_file(target, self.build_lib)
+
+
+class TomoPeriInstall(install):
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.build_scripts = None
+
+    def finalize_options(self):
+        install.finalize_options(self)
+        self.set_undefined_options('build', ('build_scripts', 'build_scripts'))
 
     def run(self):
-        nvcc_o = self._call(NVCC + ' -V')
-        if nvcc_o is not None:
-            nvcc_version = nvcc_o.split('release ')[-1].strip()
-        else:
-            raise SystemError("Nvidia's CUDA-compiler 'nvcc' can't be " \
-                          "found.")
-        print "Compiling CUDA module using nvcc %s..." % nvcc_version
+        # run original install code
+        install.run(self)
 
-        bits, linkage = platform.architecture()
-        if bits == '32bit':
-            bit_flag = ' -m32'
-        elif bits == '64bit':
-            bit_flag = ' -m64'
-        else:
-            print >>sys.stderr, "Can't detect platform, using 32bit"
-            bit_flag = ' -m32'
-
-        nvcc_cmd = NVCC + bit_flag + ' -c -arch=sm_20 '\
-                                 ' ./src/pml_cuda_kernel.cu' \
-                                 ' --compiler-options ''-fPIC'''
-        print "Executing '%s'" % nvcc_cmd
-        subprocess.check_call(nvcc_cmd, shell=True)
-
-        print "Building modules..."
-        build_ext.run(self)
+        # install TomoPeri libraries
+        self.copy_tree(self.build_lib, self.install_lib)
 
 
-class GPUCleaner(clean):
-
-    def _unlink(self, node):
-        try:
-            if os.path.isdir(node):
-                os.rmdir(node)
-            else:
-                os.unlink(node)
-        except OSError:
-            pass
-
-    def run(self):
-        print "Removing temporary files and pre-built GPU-kernels..."
-        try:
-            for f in ('pml_cuda_kernel.o'):
-                self._unlink(f)
-        except Exception, (errno, sterrno):
-            print >>sys.stderr, "Exception while cleaning temporary " \
-                                "files ('%s')" % sterrno
-        clean.run(self)
-
-class PHIBuilder(build_ext):
-
-    def _call(self, comm):
-        p = subprocess.Popen(comm, stdout=subprocess.PIPE, shell=True)
-        stdo, stde = p.communicate()
-        if p.returncode == 0:
-            return stdo
-        else:
-            print >>sys.stderr, "%s\nFailed to execute command '%s'" % \
-                                (stde, comm)
-            return None
-
-    def _makedirs(self, pathname):
-        try:
-            os.makedirs(pathname)
-        except OSError:
-            pass
-
-    def run(self):
-        icpc_o = self._call(ICPC + ' -V')
-        if icpc_o is not None:
-            icpc_version = icpc_o.split('Version ')[-1].strip()
-        else:
-            raise SystemError("Intel's phi-compiler 'icpc' can't be " \
-                          "found.")
-        print "Compiling ICPC module using icpc %s..." % icpc_version
-
-        icpc_cmd = ICPC + ' -c -O3 -fPIC src/xeon_phi/utils_cilk.cpp' \
-                                 ' src/xeon_phi/pml_quad_cilk.cpp src/xeon_phi/pml_hybrid_cilk.cpp' \
-                                 ' src/xeon_phi/ospml_quad_cilk.cpp src/xeon_phi/ospml_hybrid_cilk.cpp' 
-
-        print "Executing '%s'" % icpc_cmd
-        subprocess.check_call(icpc_cmd, shell=True)
-
-        print "Building modules..."
-        build_ext.run(self)
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-class PHICleaner(clean):
-
-    def _unlink(self, node):
-        try:
-            if os.path.isdir(node):
-                os.rmdir(node)
-            else:
-                os.unlink(node)
-        except OSError:
-            pass
-
-    def run(self):
-        print "Removing temporary files and pre-built GPU-kernels..."
-        try:
-            for f in (['utils_cilk.o', 'pml_quad_cilk.o', 'pml_hybrid_cilk.o', 'ospml_quad_cilk.o', 'ospml_hybrid_cilk.o']):
-                self._unlink(f)
-        except Exception, (errno, sterrno):
-            print >>sys.stderr, "Exception while cleaning temporary " \
-                                "files ('%s')" % sterrno
-        clean.run(self)
-
-
-cuda_extension = Extension('lib.pml_cuda',
-                    libraries = ['cuda','cudart', 'z'],
-                    extra_objects = ['pml_cuda_kernel.o'],
-                    sources = ['./src/pml_cuda.c'],
-                    include_dirs = NVIDIA_INC_DIRS,
-                    library_dirs =[CUDALIB],
-                    extra_compile_args = CUDA_EXTRA_COMPILE_ARGS)
-
-icpc_extension = Extension('lib.recon_phi',
-                    libraries = ['iomp5'],
-                    extra_objects = ['utils_cilk.o', 'pml_quad_cilk.o', 'pml_hybrid_cilk.o', 'ospml_quad_cilk.o', 'ospml_hybrid_cilk.o'],
-                    sources = ['./src/xeon_phi/recon.cpp'],
-                    include_dirs = INTEL_INC_DIRS,
-                    library_dirs =[ICPCLIB],
-                    extra_compile_args = ICPC_EXTRA_COMPILE_ARGS)
-
-setup_icpc_args = dict(
+setup_args = dict(
         name = 'tomopy_peri',
         version = VERSION,
         packages=find_packages(),
@@ -234,11 +117,14 @@ setup_icpc_args = dict(
                'Programming Language :: Python',
                'Topic :: Security'],
         platforms = ['any'],
-        ext_modules = [icpc_extension],
-        cmdclass = {'build_ext': PHIBuilder, 'clean': PHICleaner},
+        cmdclass={
+            'build': TomoPeriBuild,
+            'install': TomoPeriInstall,
+        },
         options = {'install': {'optimize': 1}, \
                     'bdist_rpm': {'requires': 'tomopy = 0.1.11'}})
 
+
 if __name__ == "__main__":
-    setup(**setup_icpc_args)
+    setup(**setup_args)
 
