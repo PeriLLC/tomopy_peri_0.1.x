a082ca721d83f5ab51dc3efcc8e42b6215bb57c6
dakefeng <dakefeng@gmail.com>
2015-08-11 20:32:45 -0400

    update copy right.

--- src/nvidia_cuda/utils_cuda.cu
+++ src/nvidia_cuda/utils_cuda.cu
@@ -1,13 +1,19 @@
+/*
+    Copyright 2014-2015 Dake Feng, Peri LLC, dakefeng@gmail.com
+    This file is part of TomograPeri.
+    TomograPeri is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    TomograPeri is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with TomograPeri.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-/**
- * Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 
